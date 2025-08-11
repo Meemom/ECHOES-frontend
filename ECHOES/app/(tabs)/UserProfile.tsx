b19312553df365ef2fd9592fd3da60cbe4dcae76
Meemom <jemima.silaen@gmail.com>
2025-08-11 14:15:08 +0700

    updated user profile

--- ECHOES/app/(tabs)/UserProfile.tsx
+++ ECHOES/app/(tabs)/UserProfile.tsx
@@ -30,6 +30,7 @@ const popularPlaylists = [
     {id: '1', name: 'Afterschool Madness', image: require('@/assets/images/war.png'), duration: '2 h 58 min', likes: 18},
     {id: '2', name: 'spiderverse vibez', image: require('@/assets/images/spiderverse.png'), duration: '14 hr 3 min', likes: 109},
     {id: '3', name: 'the last scene of a romcom', image: require('@/assets/images/romcom.png'), duration: '7 hr 6 min', likes: 22},
+    {id: '4', name: 'bling ring', image: require('@/assets/images/coolgirl.jpg'), duration: '1 hr 2 min', likes: 16}
 ];
 
 export default function UserProfile() {
