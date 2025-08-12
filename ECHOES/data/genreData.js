f7cdb413785793e84295c45cbc12e231ffb54636
Meemom <jemima.silaen@gmail.com>
2025-08-12 16:24:12 +0700

    added genre data

---
+++ ECHOES/data/genreData.js
@@ -0,0 +1,57 @@
+export const genreInfo = [
+    {
+        genre: 'Pop',
+        artistnum: 54,
+        listeningShare: 25, 
+    },
+    {
+        genre: 'Rock',
+        artistnum: 42,
+        listeningShare: 18,
+    },
+    {
+        genre: 'Hip-Hop',
+        artistnum: 37,
+        listeningShare: 15,
+    },
+    {
+        genre: 'R&B',
+        artistnum: 29,
+        listeningShare: 10,
+    },
+    {
+        genre: 'Jazz',
+        artistnum: 16,
+        listeningShare: 5,
+    },
+    {
+        genre: 'Classical',
+        artistnum: 9,
+        listeningShare: 3,
+    },
+    {
+        genre: 'Electronic',
+        artistnum: 25,
+        listeningShare: 8,
+    },
+    {
+        genre: 'Country',
+        artistnum: 14,
+        listeningShare: 4,
+    },
+    {
+        genre: 'Reggae',
+        artistnum: 11,
+        listeningShare: 3,
+    },
+    {
+        genre: 'Indie',
+        artistnum: 33,
+        listeningShare: 7,
+    },
+    {
+        genre: 'Metal',
+        artistnum: 8,
+        listeningShare: 2,
+    }
+];
