9796f20ae9810cf20419c58bc47338e85b6c298b
Meemom <jemima.silaen@gmail.com>
2025-08-22 14:57:03 +0700

    random colors for search page

--- ECHOES/app/(tabs)/SearchPage.tsx
+++ ECHOES/app/(tabs)/SearchPage.tsx
@@ -19,21 +19,66 @@ import { useThemeColor } from '@/hooks/useThemeColor';
 import { Ionicons } from '@expo/vector-icons';
 import Svg, { Defs, RadialGradient, Stop, Circle } from 'react-native-svg';
 import { useRouter } from 'expo-router';
-import { BackgroundGlow } from './HomePage';
+import { BackgroundGlow, Section } from './HomePage';
 import { MaterialIcons } from '@expo/vector-icons';
+
+// fake new releases
+const NewReleases =[
+    {id: 1, title: 'A Little More', image: require('@/assets/images/alittlemore.jpeg')},
+    {id: 2, title: 'The Subway', image: require('@/assets/images/thesubway.jpeg')},
+    {id: 3, title: 'act right', image: require('@/assets/images/actright.jpeg')},
+];
+
+function seededRandom(seed) {
+    let x = Math.sin(seed) * 10000;
+    return x - Math.floor(x);
+}
+
+function getDailyColors(numTiles) {
+    const today = new Date().toISOString().slice(0, 10); // e.g. "2025-08-19"
+    const seed = parseInt(today.replace(/-/g, "")); // use date as seed
+    const colors = [];
+
+    for (let i = 0; i < numTiles; i++) {
+        const r = Math.floor(seededRandom(seed + i) * 255);
+        const g = Math.floor(seededRandom(seed + i + 1) * 255);
+        const b = Math.floor(seededRandom(seed + i + 2) * 255);
+        colors.push(`rgb(${r},${g},${b})`);
+  }
+
+  return colors;
+}
+
+function RandomColors() {
+    return (
+
+    );
+}
 
 export default function SearchPage() {
     return (
         <View style={{ flex: 1, backgroundColor: '#121212' }}>
             <BackgroundGlow colors={['#D062FF', '#44E7E7', 'white']}/>
             <ScrollView style={styles.container}>
-                <View style={[styles.searchBar, { flexDirection: 'row', margin: 30 }]}>
+                <View style={[styles.searchBar, { flexDirection: 'row' }]}>
                     <TouchableOpacity>
                         <MaterialIcons name="search" size={20} color="black" style={{ marginRight: 10 }}/>
                     </TouchableOpacity>
                     <TextInput 
                     placeholder='Search up a song!'/>
                 </View>
+
+                {/* pick a color */}
+                <View>
+                    <ThemedText style={styles.headerTitle}>Pick a color for a random song</ThemedText>
+                    <RandomColors />
+
+                </View>
+
+                {/* new releases */}
+                <Section title="New Releases" data={NewReleases}/>
+
+                {/* TODO: unfamiliar genres */}
 
 
             </ScrollView>
@@ -49,6 +94,14 @@ const styles = StyleSheet.create({
         width: '80%',
         alignSelf: 'center',
         padding: 10,
+        marginTop: 10, 
+        marginBottom: 10
+    },
+    headerTitle: {
+        fontFamily: 'InterBold',
+        fontSize: 24, 
+        color: 'white',
+        alignSelf: 'center',
     }
 
 });
