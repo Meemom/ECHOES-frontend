a53996a4af677c1547a77ee875757c00616bdbf0
Meemom <jemima.silaen@gmail.com>
2025-08-22 15:39:13 +0700

    updated user settings

--- ECHOES/app/screens/UserProfileScreens/UserSettings.tsx
+++ ECHOES/app/screens/UserProfileScreens/UserSettings.tsx
@@ -18,11 +18,81 @@ import { useRouter } from 'expo-router';
 import { userInfo, popularPlaylists } from '@/data/userData';
 
 export default function UserSettings() {
-    return (
+    const router = useRouter();
 
+    return (
+        <ScrollView contentContainerStyle={styles.container}>
+            <TouchableOpacity
+            style={styles.closeButton}
+            onPress={() => router.push('/(tabs)/UserProfile')}
+            >
+                <MaterialIcons name="close" size={30} color="white"/>
+            </TouchableOpacity>
+
+            <View style={styles.section}>
+                <ThemedText style={styles.headerTitle}>Account Settings</ThemedText>
+                <View style={styles.settingsCard}>
+                    <TouchableOpacity>
+                        <ThemedText style={styles.settingsText}>Account & Profile</ThemedText>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <ThemedText style={styles.settingsText}>Music & Review Preferences</ThemedText>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <ThemedText style={styles.settingsText}>Payments & Subscriptions</ThemedText>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <ThemedText style={styles.settingsText}>Notifications & Privacy</ThemedText>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <ThemedText style={styles.settingsText}>App Preferences</ThemedText>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <ThemedText style={styles.settingsText}>Help & Support</ThemedText>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+
+
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
+    container: { 
+        flex: 1,
+        backgroundColor: '#101010'
+    },
+    closeButton: {
+        position: 'absolute',
+        padding: 30, 
+    },
+    section: {
+        marginTop: 50,
+        padding: 10,
+    },
+    headerTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white',
+        fontFamily: 'InterBold',
+        padding: 20,
+    },
+    settingsCard: {
+        backgroundColor: '#1F1F1F',
+        borderRadius: 20,
+        paddingHorizontal: 15,
+        paddingVertical: 10, 
+        margin: 10, 
+        width: '90%',
+    },
+    settingsText: {
+        fontFamily: 'InterMedium',
+        fontSize: 20,
+        color: 'white',
+        padding: 10,
+    }
+
 
 });
